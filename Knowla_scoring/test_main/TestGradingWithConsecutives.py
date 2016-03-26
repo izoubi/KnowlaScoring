4dd425e5fae46852b8662c6aac760f934a2527a5
izoubi <izoubi@bu.edu>
2016-03-26 16:07:56 -0500

    test_consecutives
    to test consecutes steps

---
+++ Knowla_scoring/test_main/TestGradingWithConsecutives.py
@@ -0,0 +1,57 @@
+__author__ = 'Ismail'
+
+from main.Grading import *
+import unittest
+
+
+class TestGrading(unittest.TestCase):
+    def setUp(self):
+        self.problem = ['a', 'b', 'c', 'd', 'e']
+
+        self.problem_answer = [['-',1,1,1,1],
+                               [0,'-',1,1,1],
+                               [0,0,'-',1,1],
+                               [0,0,0,'-',1],
+                               [0,0,0,0,'-']]
+        self.consecutive_steps = {'a': [3, 1, 1, 4],
+                                  'b': [3, 2, 2, 4],
+                                  'c': [5, 4, 3, 5]}
+
+        self.a = GradingProblem(self.problem, self.problem_answer, self.consecutive_steps)
+        self.a.problem_shuffled = ['b', 'a', 'e', 'c', 'd']
+        self.user_solution_matrix = [[0 for x in range(len(self.problem))] for x in range(len(self.problem))]
+        self.a.user_input = [2, 4, 1, 3, 5]
+        self.a.get_user_answer_steps()
+        self.a.get_accurate_user_input()
+        self.a.populate_user_answer_matrix_diagonal()
+        self.a.populate_user_answer_matrix()
+
+        self.problem_answer = [['-',1,1,1,1],
+                               [0,'-',1,1,1],
+                               [0,0,'-',1,1],
+                               [0,0,0,'-',1],
+                               [0,0,0,0,'-']]
+
+        self.user_score, self.total_score, self.score_percentage = self.a.get_user_score()
+        self.notes = self.a.get_user_feedback()
+
+    def test_user_answer_steps(self):
+        self.assertEqual(['a', 'c', 'b', 'e', 'd'], self.a.user_answer_steps)
+
+    def test_accurate_user_input(self):
+        self.assertEqual([1, 3, 2, 5, 4], self.a.accurate_user_input)
+
+    def test_user_answer_matrix(self):
+        self.assertEqual([['-', 1, 1, 1, 1],
+                          [0, '-', 0, 1, 1],
+                          [0, 1, '-', 1, 1],
+                          [0, 0, 0, '-', 0],
+                          [0, 0, 0, 1, '-']], self.a.user_answer_matrix)
+
+    def test_user_score(self):
+        self.assertEqual(6, self.user_score)
+        self.assertEqual(10, self.total_score)
+        self.assertEqual(60, self.score_percentage)
+
+    def test_user_feedback(self):
+        self.assertEqual(['32', '54'], self.notes)
